f8adf1351d7eb7e72755f253a5b03f2eaaadc5d6
gs256 <regularnetuser@gmail.com>
2022-01-08 18:15:28 +0300

    Implemented topmost status toggling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <Windows.h>
+
+HWND GetFocusedWindow()
+{
+    return GetForegroundWindow();
+}
+
+bool TryGetWindowTopmostStatus(HWND windowHandle, bool& outIsTopmost)
+{
+    if (!windowHandle)
+        return false;
+
+    outIsTopmost = GetWindowLong(windowHandle, GWL_EXSTYLE) & WS_EX_TOPMOST;
+    return true;
+}
+
+void ToggleFocusedWindowTopmost()
+{
+    HWND focusedWindow = GetFocusedWindow();
+    bool isTopmost;
+
+    if (TryGetWindowTopmostStatus(focusedWindow, isTopmost))
+    {
+        HWND zFlag = isTopmost ? HWND_NOTOPMOST : HWND_TOPMOST;
+        SetWindowPos(focusedWindow, zFlag, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, world\n";
+    ToggleFocusedWindowTopmost();
+    std::cout << "Toggled topmost flag\n";
 }
