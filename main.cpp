3d633daf8965d55bc51c01da5e35f8783966b749
gs256 <regularnetuser@gmail.com>
2022-01-09 11:38:47 +0300

    Set up keyboard hook

--- main.cpp
+++ main.cpp
@@ -27,8 +27,46 @@ void ToggleFocusedWindowTopmost()
     }
 }
 
+void OnKeyDown(unsigned long keyCode)
+{
+    std::cout << keyCode << " keydown\n";
+}
+
+void OnKeyUp(unsigned long keyCode)
+{
+    std::cout << keyCode << " keyup\n";
+}
+
+LRESULT CALLBACK CheckShortcut(int iCode, WPARAM wParam, LPARAM lParam)
+{
+    PKBDLLHOOKSTRUCT pHook = (PKBDLLHOOKSTRUCT)lParam;
+    DWORD keyCode = pHook->vkCode;
+
+    if (wParam == WM_KEYDOWN)
+        OnKeyDown(keyCode);
+    else if (wParam == WM_KEYUP)
+        OnKeyUp(keyCode);
+
+    return CallNextHookEx(NULL, iCode, wParam, lParam);
+}
+
+void StartKeyHook()
+{
+    HHOOK keyHook = SetWindowsHookEx(WH_KEYBOARD_LL, CheckShortcut, NULL, NULL);
+    MSG msg = { 0 };
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(keyHook);
+}
+
 int main(int argc, char* argv[])
 {
-    ToggleFocusedWindowTopmost();
-    std::cout << "Toggled topmost flag\n";
+    // ToggleFocusedWindowTopmost();
+    StartKeyHook();
+    std::cout << "Starting keyboard hook\n";
 }
