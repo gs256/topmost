ba72db782aeb9f210a3a5535e5f97d1470ddcf52
gs256 <regularnetuser@gmail.com>
2022-01-09 22:58:47 +0300

    Changed to winmain entry point

--- src/winmain.cpp
+++ src/winmain.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <Windows.h>
-#include <optional>
+#include <windows.h>
 #include "keyhook.hpp"
 #include "core.hpp"
 
 void ToggleTopmost()
 {
     Core::ToggleFocusedWindowTopmost();
-
-    if (Core::IsFocusedWindowTopmost())
-        std::cout << "topmost\n";
-    else
-        std::cout << "notopmost\n";
 }
 
 void Exit()
@@ -19,11 +13,11 @@ void Exit()
     std::exit(0);
 }
 
-int main(int argc, char* argv[])
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
     KeyHook keyHook;
     keyHook.RegisterShortcut(MOD_ALT, 0x50, ToggleTopmost);
     keyHook.RegisterShortcut(MOD_ALT | MOD_CONTROL, 0x50, Exit);
-    std::cout << "Starting keyboard hook\n";
     keyHook.Listen();
+    return 0;
 }
