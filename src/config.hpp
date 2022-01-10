d41aaa402865842d17c9add406efd3865cc41609
gs256 <regularnetuser@gmail.com>
2022-01-10 18:02:11 +0300

    Prevented multiple instances from running

---
+++ src/config.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Config
+{
+    // Used to prevent running multiple instances.
+    // `Global` prefix for global event
+    const char* WindowsEventName = "Global\\topmost";
+}
--- src/winmain.cpp
+++ src/winmain.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include "keyhook.hpp"
 #include "core.hpp"
+#include "config.hpp"
 
 void ToggleTopmost()
 {
@@ -13,11 +14,28 @@ void Exit()
     std::exit(0);
 }
 
+bool AnotherInstanceRunning()
+{
+    HANDLE globalEvent = CreateEvent(NULL, false, false, Config::WindowsEventName);
+
+    if (globalEvent == NULL)
+        return false;
+
+    if (GetLastError() == ERROR_ALREADY_EXISTS) 
+        return true;
+
+    return false;
+}
+
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
+    if (AnotherInstanceRunning())
+        return 1;
+
     KeyHook keyHook;
     keyHook.RegisterShortcut(MOD_ALT, 0x50, ToggleTopmost);
     keyHook.RegisterShortcut(MOD_ALT | MOD_CONTROL, 0x50, Exit);
     keyHook.Listen();
+
     return 0;
 }
